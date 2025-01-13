6bd426b85f9932e53654f477341a3f548fc11a09
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-01-13 17:36:09 -0300

    Initial Commit

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,4 +1,60 @@
 package application;
 
+import entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+        int accountNumber = input.nextInt();
+        input.nextLine();
+        System.out.print("Enter account holder: ");
+        String accountHolder = input.nextLine();
+        System.out.print("Is there an initial deposit (y/n)? ");
+        char response = input.next().charAt(0);
+
+        Account account;
+
+        if (response == 'y' || response == 'Y') {
+            System.out.print("Enter initial deposit value: ");
+            double initialDeposit = input.nextDouble();
+            account = new Account(accountNumber, accountHolder, initialDeposit);
+        }
+        else {
+            account = new Account(accountNumber, accountHolder);
+        }
+
+        System.out.println("Account data:");
+        System.out.print("Account " + account.getAccountNumber());
+        System.out.print(", Holder: " + account.getAccountHolder());
+        System.out.printf(", Balance: $ %.2f%n", account.getBalance());
+
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        double depositValue = input.nextDouble();
+        account.depositValue(depositValue);
+        System.out.println("Update account data:");
+        System.out.print("Account " + account.getAccountNumber());
+        System.out.print(", Holder: " + account.getAccountHolder());
+        System.out.printf(", Balance: $ %.2f%n", account.getBalance());
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double withdrawValue = input.nextDouble();
+        account.withdrawValue(withdrawValue);
+        System.out.println("Update account data:");
+        System.out.print("Account " + account.getAccountNumber());
+        System.out.print(", Holder: " + account.getAccountHolder());
+        System.out.printf(", Balance: $ %.2f%n", account.getBalance());
+
+        input.close();
+
+    }
 }
--- src/main/java/entities/Account.java
+++ src/main/java/entities/Account.java
@@ -1,4 +1,49 @@
 package entities;
 
 public class Account {
+
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public Account() {
+    }
+
+    public Account(int accountNumber, String accountHolder, double initialDeposit) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialDeposit;
+    }
+
+    public Account(int accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void depositValue(double depositValue) {
+        this.balance += depositValue;
+    }
+
+    public void withdrawValue(double withdrawValue) {
+        this.balance -= (withdrawValue + 5.0);
+    }
+
+
+
 }
